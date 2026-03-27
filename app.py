e6b90cc8d8317728e9554061b551d9a221fadc99
PawarSakshi777 <sp2272901@gmail.com>
2026-03-27 19:32:26 +0530

    Add MYSQL_PORT configuration to app
    as earlier it was using port 3306 earling which led to error so changed into this

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'hotel_management')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', '3307'))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
